5efd7ded79d30adcc9c6e3e43ce0da40602ead6b
Jon Scott <jhscott12@gmail.com>
2024-03-13 22:32:44 -0400

    3/13 - Added profile images, new component AboutSection. Updated HomePage layout and globals.css for custom class

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,12 @@
 import HeroSection from "@/components/HeroSection";
 import CallToAction from "@/components/CallToAction";
+import AboutSection from "@/components/AboutSection";
 
 const HomePage = () => {
   return (
     <>
       <HeroSection />
+      <AboutSection />
       <CallToAction />
     </>
   );
---
+++ components/AboutSection.jsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import profilePic from "@/assets/images/profile.jpg";
+
+const AboutSection = () => {
+  return (
+    <section>
+      {/* <!-- Container for About Section --> */}
+      <div className="flex flex-col gap-y-40 py-12 md:w-3/4 md:mx-auto lg:w-2/3">
+        {/* About Section Author */}
+        <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-x-36">
+          <p className="mt-10 lg:mt-0 order-last relative leading-relaxed quote-marks text-2xl sm:px-5 italic text-black">
+            Our team is knowledgeable and compassionate about providing
+            homeowners with the best solutions.
+          </p>
+          {/* About Image */}
+          <div>
+            <div className="mx-auto">
+              {/* <img className="w-full h-full object-cover" src="./images/Testimonials/man.png" alt=""> */}
+              <Image
+                src={profilePic}
+                width={300}
+                height={300}
+                className="rounded-lg mx-auto"
+              />
+            </div>
+            <div className="flex flex-col mt-1 text-sm lg:text-base">
+              <span className="text-right font-semibold text-black mx-auto text-2xl pt-3">
+                Michele Waters
+              </span>
+              <span className="text-right text-blue-400 mx-auto">
+                President & Founder, N&J Capital
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default AboutSection;
