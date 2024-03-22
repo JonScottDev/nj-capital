9596315256eb628d5daf9ca39ac3f1c2fa2f8bc9
Jon Scott <jhscott12@gmail.com>
2024-03-22 10:23:11 -0400

    3/22 - Updated .next build

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import logo from "@/public/images/logow.svg";
+import logo from "@/assets/images/logow.svg";
 import { FaInstagram, FaTwitter, FaFacebookF } from "react-icons/fa";
 
 const Footer = () => {
